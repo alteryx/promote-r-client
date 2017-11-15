a7bd6c103acc3741afe73ec7068a707c69f44f37
coristig <colin.ristig@gmail.com>
2017-11-15 10:41:41 -0500

    linting and changes for CRAN

--- R/promote.R
+++ R/promote.R
@@ -24,23 +24,24 @@ promote$model.require <- function() {
 #' @examples
 #' promote.config <- c(
 #'  username = "your username",
-#'  apikey = "your apikey"
+#'  apikey = "your apikey",
+#'  env="http://ip_of_alteryx_promote.com"
 #' )
 #' \dontrun{
 #' promote.predict_raw("irisModel", iris)
 #' }
 promote.predict_raw <- function(model_name, data, model_owner, raw_input = FALSE, silent = TRUE) {
   usage <- "usage:  promote.predict(<model_name>,<data>)"
-  if(missing(model_name)){
-    stop(paste("Please specify the model name you'd like to call",usage,sep="\n"))
+  if (missing(model_name)){
+    stop(paste("Please specify the model name you'd like to call", usage, sep = "\n"))
   }
-  if(missing(data)){
-    stop(paste("You didn't pass any data to predict on!",usage,sep="\n"))
+  if (missing(data)){
+    stop(paste("You didn't pass any data to predict on!", usage, sep = "\n"))
   }
   AUTH <- get("promote.config")
   if ("env" %in% names(AUTH)) {
     user <- AUTH[["username"]]
-    if(!missing(model_owner)){
+    if (!missing(model_owner)){
       user <- model_owner
     }
     endpoint <- sprintf("%s/models/%s/", user, model_name)
@@ -150,7 +151,7 @@ promote.library <- function(name, src="CRAN", version=NULL, user=NULL, install=T
   # If a vector of CRAN packages is passed, add each of them
   if (length(name) > 1) {
     for (n in name) {
-      promote.library(n, install=install)
+      promote.library(n, install = install)
     }
     return()
   }
@@ -198,7 +199,7 @@ promote.library <- function(name, src="CRAN", version=NULL, user=NULL, install=T
 #' }
 promote.unload <- function(name) {
   deps <- promote$dependencies
-  promote$dependencies <- deps[deps$importName != name,]
+  promote$dependencies <- deps[deps$importName != name, ]
   set.model.require()
 }
 
@@ -228,10 +229,9 @@ promote.unload <- function(name) {
 #' \dontrun{
 #' promote.library("randomForest")
 #' promote.deploy("irisModel")
-#' promote.deploy("irisModelCustomImage")
 #' }
 promote.deploy <- function(model_name, confirm=TRUE, custom_image=NULL) {
-  if(missing(model_name)){
+  if (missing(model_name)){
     stop("Please specify 'model_name' argument")
   }
   if (length(grep("^[A-Za-z0-9]+$", model_name))==0) {
@@ -239,7 +239,7 @@ promote.deploy <- function(model_name, confirm=TRUE, custom_image=NULL) {
   }
   img.size.mb <- check.image.size()
   AUTH <- get("promote.config")
-  if (length(AUTH)==0) {
+  if (length(AUTH) == 0) {
     stop("Please specify your account credentials using promote.config.")
   }
 
@@ -258,7 +258,7 @@ promote.deploy <- function(model_name, confirm=TRUE, custom_image=NULL) {
     } else {
       url <- sprintf("http://%s/api/deploy/R", env)
     }
-    image_file <- tempfile(pattern="scienceops_deployment")
+    image_file <- tempfile(pattern = "scienceops_deployment")
 
     all_objects <- promote.ls()
     # Consolidate local environment with global one
