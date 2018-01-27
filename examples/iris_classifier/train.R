d5f2c1beb38c6c6c1b7900da490efc9f829ce757
coristig <colin.ristig@gmail.com>
2018-01-27 14:00:59 -0700

    added iris example for designer

---
+++ examples/iris_classifier/train.R
@@ -0,0 +1,32 @@
+library(promote)
+library(randomForest)
+
+df <- read.csv("./iris.csv")
+
+# rename columns
+names(df) <- c("sepal_length", "sepal_width", "petal_length", "petal_width","class")
+head(df)
+
+Rf_fit<-randomForest(formula=class~., data=df)
+
+model.predict <- function(data) {
+  pred <- predict(Rf_fit, newdata=data.frame(data))
+  result <- data.frame(prediction=pred)
+  result 
+}
+
+testdata <- df[1,1:4]
+model.predict(testdata)
+
+# {"sepal_length":5.1,"sepal_width":3.5,"petal_length":1.4,"petal_width":0.2}
+
+promote.library("randomForest")
+promote.config  <- c(
+  username="USERNAME",
+  apikey="APIKEY",
+  env="PROMOTE_URL"
+)
+
+promote.deploy("IrisClassifier", confirm = FALSE)
+
+score <-promote.predict("IrisClassifier", testdata)
