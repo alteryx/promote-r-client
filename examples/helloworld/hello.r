9a3acdc64d61ef6cb62838a58b13959db4233fa4
coristig <colin.ristig@gmail.com>
2018-03-20 21:42:40 -0500

    more docs

--- examples/helloworld/hello.r
+++ examples/helloworld/hello.r
@@ -1,20 +1,25 @@
+# load the promote library
 library(promote)
 
+# create our predict function
+# this is what is executed when the REST API is called
+# `request` is a json object, parsed by jsonlite::fromJSON()
 model.predict <- function(request) {
   me <- request$name
   greeting <- paste0("Hello ", me, "!")
   greeting
 }
 
-testJSON <- '{"name": "colin"}'
-test <- rjson::fromJSON(testJSON)
+# test our predict function locally
+# if this doesn't work locally, it wont work on Promote either
+model.predict(jsonlite::fromJSON('{"name": "colin"}'))
 
-model.predict(test)
-
+# Setup the URL and authentication for deployment
 promote.config  <- c(
   username = "USERNAME",
   apikey = "APIKEY",
   env = "PROMOTE_URL"
 )
 
+# name and deploy our model
 promote.deploy("HelloWorld", confirm = FALSE)
