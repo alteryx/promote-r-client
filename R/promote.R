f29524ff9ee77a189633425d44d31c6d954b4e7e
Matthew Smith <msmith@alteryx.com>
2018-10-30 13:00:17 -0400

    Added documentation for new params in promote.library and add.dependency

--- R/promote.R
+++ R/promote.R
@@ -136,6 +136,9 @@ promote.predict <- function(model_name, data, model_owner, raw_input = FALSE, si
 #' @param src source from which the package will be installed on Promote (github or CRAN)
 #' @param version version of the package to be added
 #' @param user Github username associated with the package
+#' @param url A valid URL pointing to a remote hosted git repository
+#' @param auth_token Personal access token string associated with a private package's repository
+#' @param branch The branch of the package to be installed
 #' @param install Whether the package should also be installed into the model on the
 #' Promote server; this is typically set to False when the package has already been
 #' added to the Promote base image.
