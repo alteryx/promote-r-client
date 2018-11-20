0b1680a489cdad2631b9e0a3eaee38f1544b6586
David Cooperberg <dcooperberg@alteryx.com>
2018-11-20 09:27:50 -0700

    deleted promote.sh first line

--- examples/rodbc-model/promote.sh
+++ examples/rodbc-model/promote.sh
@@ -1,4 +1,3 @@
-
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
